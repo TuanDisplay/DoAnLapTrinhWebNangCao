f527e12b57fd1fe38b0fc0b5529ffc1855554918
Minh Thong <minhthongduongnguyen7@gmail.com>
2023-11-08 16:48:36 +0700

    Day file do an len github

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'thongduong.esrspqb.mongodb.net',
+    DB_USER: 'thongduong',
+    DB_PASS: 'thongduong',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'thongduongclone@gmail.com',
+    EMAIL_PASS: 'Clone1!!',
+    JWT_SECRET: 'admin',
+    JWT_EXPIRES: '900000000000000000000000000000000000000000000',
+}
+module.exports = MyConstants;
